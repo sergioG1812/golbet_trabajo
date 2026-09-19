325eed38a05a0ef87ae2bc859eb81e30cdcec3e7
sergioG1812 <sergioa.gonzaleza@outlook.com>
2026-09-19 17:35:16 -0500

    Add entidades de dominio, AppDbContext y migración inicial (Módulo 2)

---
+++ GolBet.Entities/Common/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolBet.Entities.Common
+{
+    public abstract class AuditableEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
+
---
+++ GolBet.Repositories/Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using GolBet.Entities;
+using GolBet.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+namespace GolBet.Repositories.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Team> Teams => Set<Team>();
+    public DbSet<Match> Matches => Set<Match>();
+    public DbSet<Bet> Bets => Set<Bet>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Team>().Property(t => t.Name).UseCollation("SQL_Latin1_General_CP1_CI_AI");
+        modelBuilder.Entity<Team>().HasIndex(t => t.Name).IsUnique();
+
+        modelBuilder.Entity<Match>().HasOne(m => m.HomeTeam).WithMany()
+            .HasForeignKey(m => m.HomeTeamId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Match>().HasOne(m => m.AwayTeam).WithMany()
+            .HasForeignKey(m => m.AwayTeamId).OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Bet>().HasOne(b => b.Match).WithMany(m => m.Bets)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var utcNow = DateTime.UtcNow;
+        foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+        {
+            if (entry.State == EntityState.Added) entry.Entity.CreatedDate = utcNow;
+            else if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.ModifiedDate = utcNow;
+                entry.Property(e => e.CreatedDate).IsModified = false;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
